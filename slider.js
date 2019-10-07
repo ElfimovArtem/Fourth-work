0a3ef386cd8b2dd53d818b69227cae65670f255a
ElfimovArtem <elfimov021@yandex.ru>
2019-10-07 11:23:17 +0300

    Redesigned slider-works, you need to edit the styles of the first-screenю

--- slider.js
+++ slider.js
@@ -1,7 +1,34 @@
-document.getElementsByClassName("next-slide").onclick = sliderNext;
-    var top = 0;
-function sliderNext() {
-    var slider__line = document.getElementsByClassName("slider__line");
-    top = top - 252;
-    slider__line.style.top = top +'px';
+/* Индекс слайда по умолчанию */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function prevSlide() {
+    showSlides(slideIndex -= 1);
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+    var i;
+    var slider = document.getElementsByClassName("slider__item");
+    if (n > slider.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slider.length
+    }
+    for (i = 0; i < slider.length; i++) {
+        slider[i].style.display = "none";
+    }
+    slider[slideIndex - 1].style.display = "block";
 }
